93a32b69e403b2c7af672ef3af1626b42512bcf9
Tijil Agrawal <77985046+Tijil2111@users.noreply.github.com>
2022-08-19 12:57:08 +0530

    Added Logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Add the favicon */}
+      <Head>
+        <link rel="shortcut icon" href="/logo.jpeg" />
+      </Head>
+      {/* Add the favicon */}
+      {/* Note that the path doesn't include "public" */}
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
