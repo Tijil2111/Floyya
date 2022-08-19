b44a2cf974e9f440cec8159cc9b84b74f38938e1
Tijil Agrawal <77985046+Tijil2111@users.noreply.github.com>
2022-08-19 16:12:28 +0530

    Changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="h-[90px] flex justify-center pt-8 text-white font-poppinsSemiBold  w-[150vw] md:w-[100%] bg-gradient-to-r from-main-light via-main-mid-1 to-main-dark">
-      Made with ❤️ by DPS Noida
+      Embrace your womanhood. It's a beautiful gift.
     </div>
   );
 }
